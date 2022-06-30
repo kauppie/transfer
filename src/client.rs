cdcea4a8fb634ee625075389a5a484c1e3b28f6e
Elias Kauppi <elkku.kauppi@gmail.com>
2022-07-01 00:29:36 +0300

    JWT validation works

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,16 @@
+mod common;
+
 use clap::Parser;
 use tokio::io::AsyncWriteExt;
+use tonic::metadata::MetadataValue;
 use tonic::transport::{Certificate, Channel, ClientTlsConfig};
+use tonic::Request;
 
+use transfer::login_client::LoginClient;
 use transfer::transfer_client::TransferClient;
-use transfer::{FileName, Path};
+use transfer::{GetFileRequest, ListFilesRequest};
+
+use crate::transfer::LoginRequest;
 
 pub mod transfer {
     tonic::include_proto!("transfer"); // The string specified here must match the proto package name
@@ -20,6 +27,7 @@ struct Args {
 enum Command {
     List(ListArgs),
     Get(GetArgs),
+    Login(LoginArgs),
 }
 
 #[derive(clap::Args, Debug)]
@@ -32,60 +40,76 @@ struct GetArgs {
     name: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(clap::Args, Debug)]
+struct LoginArgs {
+    username: String,
+    password: String,
+}
+
+type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), StdError> {
     // Parse command line arguments.
     let args = Args::parse();
 
-    // Build a runtime for asynchronous requests.
-    let runtime = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .expect("couldn't create tokio runtime");
+    // Read server's CA certificate.
+    let pem = tokio::fs::read("dev/ca.pem").await?;
+    let ca = Certificate::from_pem(pem);
 
-    // Run asynchronous runtime.
-    runtime.block_on(async move {
-        // Read server's CA certificate.
-        let pem = tokio::fs::read("dev/ca.pem").await?;
-        let ca = Certificate::from_pem(pem);
+    // Configure TLS client with server settings.
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(ca)
+        .domain_name("example.com");
 
-        // Configure TLS client with server settings.
-        let tls = ClientTlsConfig::new()
-            .ca_certificate(ca)
-            .domain_name("example.com");
+    // Create and connect a TLS channel to the server.
+    let channel = Channel::from_static("http://[::1]:50051")
+        .tls_config(tls)?
+        .connect()
+        .await?;
 
-        // Create and connect a TLS channel to the server.
-        let channel = Channel::from_static("http://[::1]:50051")
-            .tls_config(tls)?
-            .connect()
-            .await?;
+    let token: MetadataValue<_> = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJoZWxsbyIsImV4cCI6MTY1NjYyODExN30.av9hf8fDRSzPzZtU6nUTpo8081UgUiWFHj-lKBefaiWQpVWVVVrzdUHqseNsNL-E-zi2q1nOFJg6plRad3Y6ngiGbH08Hye1T5HrgaBzS5QBhBVYfoFDPdkSojX5KLHnwApi9B1G2mN3UI_SWScPJBxhOfCxjin1RL7u1XLqBI7kGMYRFUiK7jSERE7ktFvGyKFsjJWrln9gLQQ6vh4cyGHE2bZeisPtqaSll1rd3iu7mG6Wy0MUCXYCJllv1YwcZl-PuaAP9woDSKXpnSNmhlQ2nbZxPhQjRXQO69DJ-oPheWkBP1dHWmZbUgzqFk5sZWM2_PwtNLdTnNoRL8Wsvg".parse()?;
 
-        // Build and connect a client for gRPC communication.
-        let mut client = TransferClient::new(channel);
+    // Create a client using the created channel.
+    let mut client =
+        TransferClient::with_interceptor(channel.clone(), move |mut req: Request<()>| {
+            req.metadata_mut().insert("authorization", token.clone());
+            Ok(req)
+        });
 
-        match args.command {
-            Command::List(args) => {
-                // Create a file list request.
-                let request = tonic::Request::new(Path { path: args.path });
-                // Make a request to the server with request and get response.
-                let response = client.list_files(request).await?;
+    let mut login_client = LoginClient::new(channel);
 
-                // Print result file names.
-                for file_name in response.into_inner().names {
-                    println!("{}", file_name);
-                }
-            }
-            Command::Get(args) => {
-                // Make a request to the server with file name request and get file response.
-                let response = client
-                    .get_file(tonic::Request::new(FileName { name: args.name }))
-                    .await?
-                    .into_inner();
+    match args.command {
+        Command::List(args) => {
+            // Create a file list request.
+            let request = tonic::Request::new(ListFilesRequest { path: args.path });
+            // Make a request to the server with request and get response.
+            let response = client.list_files(request).await?;
 
-                let mut file = tokio::fs::File::create("output").await?;
-                file.write_all(&response.content).await?;
+            // Print result file names.
+            for file_name in response.into_inner().names {
+                println!("{}", file_name);
             }
         }
+        Command::Get(args) => {
+            // Make a request to the server with file name request and get file response.
+            let response = client
+                .get_file(tonic::Request::new(GetFileRequest { name: args.name }))
+                .await?
+                .into_inner();
 
-        Ok(())
-    })
+            let mut file = tokio::fs::File::create("output").await?;
+            file.write_all(&response.content).await?;
+        }
+        Command::Login(args) => {
+            let request = tonic::Request::new(LoginRequest {
+                username: args.username,
+                password: args.password,
+            });
+            let response = login_client.login(request).await?;
+            println!("{:?}", response);
+        }
+    }
+
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,22 @@
+mod common;
+
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
 use tonic::{
     transport::{Identity, Server, ServerTlsConfig},
     Request, Response, Status,
 };
 
-use transfer::transfer_server::{Transfer, TransferServer};
-use transfer::{FileName, FileNames, FileResponse, Path};
+use transfer::{
+    login_server::{Login, LoginServer},
+    transfer_server::{Transfer, TransferServer},
+    GetFileRequest, GetFileResponse, ListFilesRequest, LoginRequest, LoginResponse,
+};
+
+use crate::transfer::ListFilesResponse;
 
 pub mod transfer {
     tonic::include_proto!("transfer"); // The string specified here must match the proto package name
@@ -15,10 +27,19 @@ type ResponseResult<T> = Result<Response<T>, Status>;
 #[derive(Debug, Default)]
 pub struct MyTransfer {}
 
+impl MyTransfer {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
 #[tonic::async_trait]
 impl Transfer for MyTransfer {
     #[tracing::instrument]
-    async fn list_files(&self, request: Request<Path>) -> ResponseResult<FileNames> {
+    async fn list_files(
+        &self,
+        request: Request<ListFilesRequest>,
+    ) -> ResponseResult<ListFilesResponse> {
         tracing::info!("got a file listing request");
 
         let mut names = Vec::new();
@@ -33,13 +54,13 @@ impl Transfer for MyTransfer {
             );
         }
 
-        let reply = FileNames { names };
+        let reply = ListFilesResponse { names };
 
         Ok(Response::new(reply))
     }
 
     #[tracing::instrument]
-    async fn get_file(&self, request: Request<FileName>) -> ResponseResult<FileResponse> {
+    async fn get_file(&self, request: Request<GetFileRequest>) -> ResponseResult<GetFileResponse> {
         tracing::info!("got a file download request");
 
         let file_name = request.into_inner().name;
@@ -48,12 +69,60 @@ impl Transfer for MyTransfer {
             .await
             .map_err(|_| Status::failed_precondition("file does not exist"))?;
 
-        let reply = FileResponse {
+        let reply = GetFileResponse {
             name: file_name,
             content: bytes,
         };
 
         Ok(Response::new(reply))
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct MyLogin {
+    logins: Arc<Mutex<HashMap<String, String>>>,
+}
+
+impl MyLogin {
+    const NAME: &'static str = "hello";
+    const PASSWORD: &'static str = "world";
+
+    pub fn new(logins: Arc<Mutex<HashMap<String, String>>>) -> Self {
+        Self { logins }
+    }
+}
+
+#[tonic::async_trait]
+impl Login for MyLogin {
+    #[tracing::instrument]
+    async fn login(&self, request: Request<LoginRequest>) -> ResponseResult<LoginResponse> {
+        tracing::info!("got a login request");
+
+        let request = request.into_inner();
+        if request.username == Self::NAME && request.password == Self::PASSWORD {
+            let claims = common::Claims {
+                sub: request.username.clone(),
+                exp: jsonwebtoken::get_current_timestamp() + 3600, // One hour validity.
+            };
+
+            let token = jsonwebtoken::encode(
+                &jsonwebtoken::Header::new(jsonwebtoken::Algorithm::RS256),
+                &claims,
+                &jsonwebtoken::EncodingKey::from_rsa_pem(include_bytes!("../dev/private.pem"))
+                    .map_err(|_| Status::internal("private key is invalid"))?,
+            )
+            .map_err(|_| Status::internal("token encoding failed"))?;
+
+            // Add username and corresponding token.
+            self.logins
+                .lock()
+                .unwrap()
+                .insert(token.clone(), request.username);
+
+            Ok(Response::new(LoginResponse { token }))
+        } else {
+            Err(Status::permission_denied("invalid credentials"))
+        }
     }
 }
 
@@ -70,29 +139,62 @@ fn setup_tracing() {
         .expect("failed to set global tracing subscriber");
 }
 
-fn main() -> Result<(), StdError> {
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("couldn't create tokio runtime");
+#[tokio::main]
+#[tracing::instrument]
+async fn main() -> Result<(), StdError> {
+    setup_tracing();
 
-    runtime.block_on(async move {
-        setup_tracing();
+    let cert = tokio::fs::read("dev/cert.pem").await?;
+    let key = tokio::fs::read("dev/cert.key").await?;
+    let identity = Identity::from_pem(&cert, &key);
 
-        let cert = tokio::fs::read("dev/cert.pem").await?;
-        let key = tokio::fs::read("dev/cert.key").await?;
+    let jwt_pub_key = tokio::fs::read("dev/public.pem").await?;
+    let decoding_key = jsonwebtoken::DecodingKey::from_rsa_pem(&jwt_pub_key)
+        .map_err(|_| Status::internal("decoding error"))?;
 
-        let identity = Identity::from_pem(cert, key);
+    lazy_static::lazy_static! {
+        static ref BEAR_REGEX: regex::Regex = regex::Regex::new(r"Bearer (?P<bearer>.*)").unwrap();
+    }
 
-        let addr = "[::1]:50051".parse()?;
-        let greeter = MyTransfer::default();
+    let addr = "[::1]:50051".parse()?;
 
-        Server::builder()
-            .tls_config(ServerTlsConfig::new().identity(identity))?
-            .add_service(TransferServer::new(greeter))
-            .serve(addr)
-            .await?;
+    // Map to store username and password combos. Just for testing.
+    let pass_storage = Arc::new(Mutex::new(HashMap::<String, String>::new()));
 
-        Ok(())
-    })
+    let my_login = MyLogin::new(pass_storage.clone());
+    let login_service = LoginServer::new(my_login);
+
+    let my_transfer = MyTransfer::default();
+    let transfer_service = TransferServer::with_interceptor(my_transfer, move |req: Request<()>| {
+        let bearer = req
+            .metadata()
+            .get("authorization")
+            .unwrap()
+            .to_str()
+            .unwrap();
+
+        let token_str = &BEAR_REGEX.captures_iter(bearer).next().unwrap()["bearer"];
+        tracing::info!("{}", token_str);
+
+        let header = jsonwebtoken::decode_header(&token_str);
+        tracing::info!("{:?}", header);
+
+        let _token_data = jsonwebtoken::decode::<common::Claims>(
+            token_str,
+            &decoding_key,
+            &jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256),
+        )
+        .map_err(|e| Status::permission_denied(format!("{}", e)))?;
+
+        Ok(Request::new(()))
+    });
+
+    Server::builder()
+        .tls_config(ServerTlsConfig::new().identity(identity))?
+        .add_service(login_service)
+        .add_service(transfer_service)
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
